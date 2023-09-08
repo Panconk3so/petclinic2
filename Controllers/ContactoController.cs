f6faf33d5bfd046b660861740a4866ae89060e79
Cristopher Nieves <cristopher_nieves@usmp.pe>
2023-09-08 18:06:07 -0500

    Grabando Contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using petclinic2.Data;
 using petclinic2.Models;
 
 namespace petclinic.Controllers
@@ -12,10 +13,12 @@ namespace petclinic.Controllers
     public class ContactoController : Controller
     {
         private readonly ILogger<ContactoController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public ContactoController(ILogger<ContactoController> logger)
+        public ContactoController(ILogger<ContactoController> logger,ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +29,9 @@ namespace petclinic.Controllers
         [HttpPost]
         public IActionResult Create(Contacto objContacto)
         {
+            _context.Add(objContacto);
+            _context.SaveChanges();
+
             ViewData["Message"] = string.Concat("Estimado " , objContacto.Name, " te estaremos contactando pronto.");
             return View("Index");
         }
