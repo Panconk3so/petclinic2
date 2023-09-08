2484c28c1addaa032e49c5533aec71dbe3c8821c
Cristopher Nieves <cristopher_nieves@usmp.pe>
2023-09-08 17:56:28 -0500

    Uso base de datos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using petclinic2.Models;
 
 namespace petclinic2.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<Contacto> DataContactos { get; set;}
 }
--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace petclinic2.Models
 {
+    [Table ("t_contacto")]
     public class Contacto
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
